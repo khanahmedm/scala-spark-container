2c5f0485314146382cf610bc37bda76b0e48fa0f
Ahmed Khan <khan.ahmed.m@outlook.com>
2022-08-19 11:30:37 -0500

    added indentations in the code in the if-else block

--- app/spark/access-log-process/src/main/scala/TopURLsAndVisitors.scala
+++ app/spark/access-log-process/src/main/scala/TopURLsAndVisitors.scala
@@ -32,88 +32,88 @@ object TopURLsAndVisitors {
     else {
     
 
-    // retrieve file name from the argument passed
-    val fileName = args(0)
-    // Setting the log level to print errors only
-    Logger.getLogger("org").setLevel(Level.ERROR)
+      // retrieve file name from the argument passed
+      val fileName = args(0)
+      // Setting the log level to print errors only
+      Logger.getLogger("org").setLevel(Level.ERROR)
 
-    // Creating a spark session
-    val spark = SparkSession
-      .builder
-      .appName("TopURLsAndVisitors")
-      .master("local[*]")
-      .getOrCreate()
+      // Creating a spark session
+      val spark = SparkSession
+        .builder
+        .appName("TopURLsAndVisitors")
+        .master("local[*]")
+        .getOrCreate()
 
-    // Loading the data file into a dataset
-    import spark.implicits._
-    val ds = spark.read
-      .option("header", "false")
-      .option("inferSchema", "false")
-      .text("../../data/" + fileName)
+      // Loading the data file into a dataset
+      import spark.implicits._
+      val ds = spark.read
+        .option("header", "false")
+        .option("inferSchema", "false")
+        .text("../../data/" + fileName)
 
-    // Creating regular expressions for  extracting data from access log
-    val contentSizeExp = "\\s(\\d+)$"
-    val statusExp = "\\s(\\d{3})\\s"
-    val generalExp = "\"(\\S+)\\s(\\S+)\\s*(\\S*)\""
-    val timeExp = "\\[(\\d{2}/\\w{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} -\\d{4})]"
-    val userExp = "(^\\S+\\.[\\S+\\.]+\\S+)\\s"
+      // Creating regular expressions for  extracting data from access log
+      val contentSizeExp = "\\s(\\d+)$"
+      val statusExp = "\\s(\\d{3})\\s"
+      val generalExp = "\"(\\S+)\\s(\\S+)\\s*(\\S*)\""
+      val timeExp = "\\[(\\d{2}/\\w{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} -\\d{4})]"
+      val userExp = "(^\\S+\\.[\\S+\\.]+\\S+)\\s"
 
-    // Applying the regular expressions to create structure from the unstructured text
-    val logsDF = ds.select(regexp_extract(col("value"), userExp, 1).alias("visitors"),
-    regexp_extract(col("value"), timeExp, 1).alias("timestamp"),
-    regexp_extract(col("value"), generalExp, 1).alias("method"),
-    regexp_extract(col("value"), generalExp, 2).alias("URL"),
-    regexp_extract(col("value"), generalExp, 3).alias("protocol"),
-    regexp_extract(col("value"), statusExp, 1).cast("Integer").alias("status"),
-    regexp_extract(col("value"), contentSizeExp, 1).cast("Integer").alias("content_size"))
-    
-    // Removing rows where there is no endpoint URL
-    val logsNoURLsDF = logsDF.filter("URL != ''")
-    
-    // Removing rows where there is no visitors
-    val logsNoVisitorsDF = logsDF.filter("visitors != ''")
+      // Applying the regular expressions to create structure from the unstructured text
+      val logsDF = ds.select(regexp_extract(col("value"), userExp, 1).alias("visitors"),
+      regexp_extract(col("value"), timeExp, 1).alias("timestamp"),
+      regexp_extract(col("value"), generalExp, 1).alias("method"),
+      regexp_extract(col("value"), generalExp, 2).alias("URL"),
+      regexp_extract(col("value"), generalExp, 3).alias("protocol"),
+      regexp_extract(col("value"), statusExp, 1).cast("Integer").alias("status"),
+      regexp_extract(col("value"), contentSizeExp, 1).cast("Integer").alias("content_size"))
+      
+      // Removing rows where there is no endpoint URL
+      val logsNoURLsDF = logsDF.filter("URL != ''")
+      
+      // Removing rows where there is no visitors
+      val logsNoVisitorsDF = logsDF.filter("visitors != ''")
 
-    // Adding a new column for date field for URL dataset - that is a substring of timestamp column
-    val logsNoURLsWithDateDF = logsNoURLsDF.withColumn("date", substring(col("timestamp"),1,11))
+      // Adding a new column for date field for URL dataset - that is a substring of timestamp column
+      val logsNoURLsWithDateDF = logsNoURLsDF.withColumn("date", substring(col("timestamp"),1,11))
 
-    // Adding a new column for date field for visitors dataset - that is a substring of timestamp column
-    val logsNoVisitorsWithDateDF = logsNoVisitorsDF.withColumn("date", substring(col("timestamp"),1,11))
+      // Adding a new column for date field for visitors dataset - that is a substring of timestamp column
+      val logsNoVisitorsWithDateDF = logsNoVisitorsDF.withColumn("date", substring(col("timestamp"),1,11))
 
-    // assigning value for 'n' as in top-n rows - the value is passed as an argument 
-    val numOfRowsToReturnForEachDay = args(1).toInt 
-    // hard-coded value for July, however, it can be made dymanic
-    val numOfDaysInMonth = 31
+      // assigning value for 'n' as in top-n rows - the value is passed as an argument 
+      val numOfRowsToReturnForEachDay = args(1).toInt 
+      // hard-coded value for July, however, it can be made dymanic
+      val numOfDaysInMonth = 31
 
-    val numOfRowsToReturnInQueryResult = numOfRowsToReturnForEachDay*numOfDaysInMonth
+      val numOfRowsToReturnInQueryResult = numOfRowsToReturnForEachDay*numOfDaysInMonth
 
-    // creating a window partitioned by date and count columns
-    val overDateCount = Window.partitionBy($"date").orderBy($"count".desc)
-    
-    /****** Finding top-n URLs *********/
-    val countURLsDF = logsNoURLsWithDateDF.groupBy("URL", "date").count()
-    
-    // applying dense rank to get the ranking for each URL for each day
-    val rankedURLsDF = countURLsDF.withColumn("ranking", dense_rank.over(overDateCount)).filter(col("ranking") <= numOfRowsToReturnForEachDay)
-    
-    // creating the final result set for top-n URLs
-    val topURLsDF = rankedURLsDF.select("*").orderBy(col("date").asc, col("count").desc)
-    
-    // show top-n URLs resulting rows and full column values
-    println("********** TOP " + numOfRowsToReturnForEachDay + " URLs for each day **********")
-    topURLsDF.show(numOfRowsToReturnInQueryResult,false)
+      // creating a window partitioned by date and count columns
+      val overDateCount = Window.partitionBy($"date").orderBy($"count".desc)
+      
+      /****** Finding top-n URLs *********/
+      val countURLsDF = logsNoURLsWithDateDF.groupBy("URL", "date").count()
+      
+      // applying dense rank to get the ranking for each URL for each day
+      val rankedURLsDF = countURLsDF.withColumn("ranking", dense_rank.over(overDateCount)).filter(col("ranking") <= numOfRowsToReturnForEachDay)
+      
+      // creating the final result set for top-n URLs
+      val topURLsDF = rankedURLsDF.select("*").orderBy(col("date").asc, col("count").desc)
+      
+      // show top-n URLs resulting rows and full column values
+      println("********** TOP " + numOfRowsToReturnForEachDay + " URLs for each day **********")
+      topURLsDF.show(numOfRowsToReturnInQueryResult,false)
 
-    /****** Finding top-n visitors *******/
-    val countVisitorsDF = logsNoVisitorsWithDateDF.groupBy("visitors", "date").count()
+      /****** Finding top-n visitors *******/
+      val countVisitorsDF = logsNoVisitorsWithDateDF.groupBy("visitors", "date").count()
 
-    // applying dense rank to get the ranking for each visitor for each day
-    val rankedVisitorsDF = countVisitorsDF.withColumn("ranking", dense_rank.over(overDateCount)).filter(col("ranking") <= numOfRowsToReturnForEachDay)
+      // applying dense rank to get the ranking for each visitor for each day
+      val rankedVisitorsDF = countVisitorsDF.withColumn("ranking", dense_rank.over(overDateCount)).filter(col("ranking") <= numOfRowsToReturnForEachDay)
 
-    // creating the final result set for top-n visitors
-    val topVisitorsDF = rankedVisitorsDF.select("*").orderBy(col("date").asc, col("count").desc)
+      // creating the final result set for top-n visitors
+      val topVisitorsDF = rankedVisitorsDF.select("*").orderBy(col("date").asc, col("count").desc)
 
-    // show top-n visitors resulting rows and full column values
-    println("********** TOP " + numOfRowsToReturnForEachDay + " visitors for each day **********")
-    topVisitorsDF.show(numOfRowsToReturnInQueryResult,false)
+      // show top-n visitors resulting rows and full column values
+      println("********** TOP " + numOfRowsToReturnForEachDay + " visitors for each day **********")
+      topVisitorsDF.show(numOfRowsToReturnInQueryResult,false)
 
     }
   }
