cc1f5ab6e22d3ed3fc68b92aff41462b9168f5b7
Ahmed Khan <khan.ahmed.m@outlook.com>
2022-08-20 14:14:48 -0500

    added checks for errors

--- app/scala/download-file/downloadFile.scala
+++ app/scala/download-file/downloadFile.scala
@@ -3,13 +3,14 @@
   Title: download file program using a URL
   Author: Ahmed M Khan
   Date Created: 8/16/22
-  Date Modified: 8/18/22
+  Date Modified: 8/20/22
   Description : This program downloads a file using a URL provided as an argument.
 **********************************************************************************/
 
 
 import sys.process._
-import java.net.{HttpURLConnection, URL}
+import java.net.{HttpURLConnection,URL} 
+import java.net.{MalformedURLException,UnknownHostException,SocketTimeoutException}
 import java.io.{File,FileReader,FileWriter}
 import java.io.FileNotFoundException
 import java.io.IOException
@@ -34,36 +35,44 @@ object downloadFile {
         println("file to download: " + downloadFileName)
 
         // specifying the download file target path
-        val outputFileName = "../../data/" + downloadFileName
-
-        // Instantiating URL object
-        val url = new URL(urlOfFileToDownload)
-
-        // Setting up the connection
-        val connection = url.openConnection().asInstanceOf[HttpURLConnection]
-        connection.setConnectTimeout(5000)
-        connection.setReadTimeout(5000)
-        connection.connect()
+        val targetDirPath = "../../data/"
+        //File targetDirPath
+        val outputFileName = targetDirPath + downloadFileName
         
-        // downloading the file - in case of error, showing the HTTP error response code
         try {
-          if (connection.getResponseCode >= 400) {
-            println("error downloading the file - HTTP error code: " + connection.getResponseCode)
-            connection.disconnect()
-        }
+          // Instantiating URL object
+          val url = new URL(urlOfFileToDownload)
+        
+          // Setting up the connection
+          val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+          connection.setConnectTimeout(5000)
+          connection.setReadTimeout(5000)
+
+          val httpResponseCode = connection.getResponseCode
+          
+          // checking reponse code for errors
+          if (httpResponseCode >= 400) {
+              println("Error: downloading the file - HTTP error code: " + httpResponseCode)
+          }
+          // checking if the target directory exists
+          else if (! new java.io.File(targetDirPath).exists) {
+            println("Error : Target directory path "+ targetDirPath +" does not exist. Please check.")
+          }
+          // connecting and downloading the file
           else {
+            connection.connect()
             println("downloading...")
             url #> new File(outputFileName) !!
-            
-            println("download completed.")
           }
         }
-        // catching any possible exceptions
+        // catching exceptions
         catch {
-         case e: java.io.IOException => println("Had an IOException trying to read that file")
-         case e: java.lang.RuntimeException => println("Runtime exception")
-         case e: java.io.FileNotFoundException => println("Couldn't find that file.")
-         case e: java.net.SocketTimeoutException => println("Connection timeout")
+          case e: java.net.MalformedURLException => println("Error: Malformed URL - please check the URL.")
+          case e: java.net.UnknownHostException => println("Error: Unknown host - please check the hostname in the URL")
+          case e: java.io.FileNotFoundException => println("Error: Couldn't find that file.")
+          case e: java.io.IOException => println("Error: Had an IOException trying to read that file")
+          case e: java.lang.RuntimeException => println("Error: Runtime exception")
+          case e: java.net.SocketTimeoutException => println("Error: Connection timeout")
         }
     }
   }
